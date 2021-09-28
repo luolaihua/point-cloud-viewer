2893775f5f88c41bee8fdd23067c47d1de9cfe79
luo <1973653885@qq.com>
2021-09-28 21:25:29 +0800

    start to work !

--- pclvisualizer.cpp
+++ pclvisualizer.cpp
@@ -1,15 +1,79 @@
-#include "pclvisualizer.h"
+﻿#include "pclvisualizer.h"
+
+#include <QDebug>
+
 #include "./ui_pclvisualizer.h"
-
-PCLVisualizer::PCLVisualizer(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::PCLVisualizer)
-{
+PCLVisualizer::PCLVisualizer(QWidget* parent) : QMainWindow(parent), ui(new Ui::PCLVisualizer) {
     ui->setupUi(this);
+    cloud_.reset(new PointCloudT);
+    connect(ui->pushButton_load, &QPushButton::clicked, this, &PCLVisualizer::loadPCDFile);
 }
 
-PCLVisualizer::~PCLVisualizer()
-{
-    delete ui;
+PCLVisualizer::~PCLVisualizer() { delete ui; }
+
+void PCLVisualizer::test() { qDebug() << "Hello World!"; }
+
+void PCLVisualizer::savePCDFile() {
+    QString filename =
+        QFileDialog::getSaveFileName(this, tr("Open point cloud"), "/home/", tr("Point cloud data(*.pcd *.ply)"));
+    PCL_INFO("File chosen: %s\n", filename.toStdString().c_str());
+
+    if (filename.isEmpty())
+        return;
+    int return_status;
+    if (filename.endsWith(".pcd", Qt::CaseInsensitive))
+        return_status = pcl::io::savePCDFileBinary(filename.toStdString(), *cloud_);
+    else if (filename.endsWith(".ply", Qt::CaseInsensitive))
+        return_status = pcl::io::savePLYFileBinary(filename.toStdString(), *cloud_);
+    else {
+        filename.append(".ply");
+        return_status = pcl::io::savePLYFileBinary(filename.toStdString(), *cloud_);
+    }
+    if (return_status != 0) {
+        PCL_ERROR("Error writing point cloud %s\n", filename.toStdString().c_str());
+        return;
+    }
 }
 
+void PCLVisualizer::loadPCDFile() {
+    QString filename =
+        QFileDialog::getOpenFileName(this, tr("Open point cloud"), "/home/", tr("Point cloud data (*.pcd *.ply)"));
+
+    PCL_WARN("File chosen: %s\n", filename.toStdString().c_str());
+    //创建一个临时的指针保存加载的点云数据
+    PointCloudT::Ptr cloud_tmp(new PointCloudT);
+    //如果为空，直接返回
+    if (filename.isEmpty())
+        return;
+
+    int return_status;
+    if (filename.endsWith(".pcd", Qt::CaseInsensitive))
+        return_status = pcl::io::loadPCDFile(filename.toStdString(), *cloud_tmp);
+    else
+        return_status = pcl::io::loadPLYFile(filename.toStdString(), *cloud_tmp);
+
+    if (return_status != 0) {
+        PCL_ERROR("Error reading point cloud %s\n", filename.toStdString().c_str());
+        return;
+    }
+    PCL_WARN("file has loaded\n");
+    // If point cloud contains NaN values, remove them before updating the visualizer point cloud
+    //    True if no points are invalid (e.g., have NaN or Inf values in any of their floating point fields).
+    //如果没有无效的点（如NAN，无效或非法的点），返回true
+    if (cloud_tmp->is_dense)
+        pcl::copyPointCloud(*cloud_tmp, *cloud_);
+    else {
+        PCL_WARN("Cloud is not dense! Non finite points will be removed\n");
+        std::vector<int> vec;
+        pcl::removeNaNFromPointCloud(*cloud_tmp, *cloud_, vec);
+    }
+    qDebug() << "The number of points :" << cloud_->points.size();
+    //    colorCloudDistances ();
+    //    viewer_->updatePointCloud (cloud_,"cloud");
+    //    viewer_->resetCamera ();
+    //    ui->qvtkWidget->update ();
+}
+
+void PCLVisualizer::chooseAxis() {}
+
+void PCLVisualizer::chooseColorMode() {}
--- pclvisualizer.h
+++ pclvisualizer.h
@@ -1,34 +1,77 @@
 ﻿#ifndef PCLVISUALIZER_H
 #define PCLVISUALIZER_H
 
+#include <QFileDialog>
 #include <QMainWindow>
 // Point Cloud Library
-//#include <pcl/point_cloud.h>
-//#include <pcl/point_types.h>
-//#include <pcl/io/ply_io.h>
-//#include <pcl/io/pcd_io.h>
-//#include <pcl/filters/filter.h>
-//#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/filters/filter.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/visualization/pcl_visualizer.h>
 
-//// Boost
-//#include <boost/math/special_functions/round.hpp>
+// Boost
+#include <boost/math/special_functions/round.hpp>
 
-//// Visualization Toolkit (VTK)
-//#include <vtkRenderWindow.h>
+// Visualization Toolkit (VTK)
+#include <vtkRenderWindow.h>
+
+//使用的点云格式
+// typedef pcl::PointXYZRGBA PointT;
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloudT;
 
 QT_BEGIN_NAMESPACE
-    namespace Ui { class PCLVisualizer; }
+namespace Ui {
+    class PCLVisualizer;
+}
 QT_END_NAMESPACE
 
-    class PCLVisualizer : public QMainWindow
-{
+class PCLVisualizer : public QMainWindow {
     Q_OBJECT
 
-        public:
-                 PCLVisualizer(QWidget *parent = nullptr);
+  public:
+    PCLVisualizer(QWidget* parent = nullptr);
     ~PCLVisualizer();
+  public slots:
+    void test();
+    //保存文件
+    void savePCDFile();
+    void loadPCDFile();
 
-private:
-    Ui::PCLVisualizer *ui;
+    //选择需要控制的坐标轴
+    void chooseAxis();
+    //选择颜色模式
+    void chooseColorMode();
+
+  protected:
+    //创建一个共享的PCLVisualizer 对象用于显示
+    pcl::visualization::PCLVisualizer::Ptr viewer_;
+    //创建一个共享指针用于保存点云
+    PointCloudT::Ptr cloud_;
+    /** @brief 坐标轴：0 = x | 1 = y | 2 = z */
+    int filtering_axis_;
+
+    /** @brief 颜色模式：Holds the color mode for @ref colorCloudDistances */
+    int color_mode_;
+
+    /** @brief Color point cloud on X,Y or Z axis using a Look-Up Table (LUT)
+     * Computes a LUT and color the cloud accordingly, available color palettes are :
+     *
+     *  Values are on a scale from 0 to 255:
+     *  0. Blue (= 0) -> Red (= 255), this is the default value
+     *  1. Green (= 0) -> Magenta (= 255)
+     *  2. White (= 0) -> Red (= 255)
+     *  3. Grey (< 128) / Red (> 128)
+     *  4. Blue -> Green -> Red (~ rainbow)
+     *
+     * @warning If there's an outlier in the data the color may seem uniform because of this outlier!
+     * @note A boost rounding exception error will be thrown if used with a non dense point cloud
+     */
+    void colorCloudDistances();
+
+  private:
+    Ui::PCLVisualizer* ui;
 };
-#endif // PCLVISUALIZER_H
+#endif  // PCLVISUALIZER_H
